9d46bb98043721f2d6d89021f961530b0897b43a
郑翔 <zhengxiang@quyiyuan.com>
2018-05-21 13:56:34 +0800

    添加 autoprefixer

--- webpack.config.js
+++ webpack.config.js
@@ -36,13 +36,17 @@ module.exports = {
       test: /\.css$/,
       use : [
         devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
-        'css-loader'
+        { loader: 'css-loader'},
+        {loader: 'postcss-loader'}
       ]
     }, {
       test: /\.scss$/,
       use : [
         devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
         'css-loader',
+        {
+          loader: 'postcss-loader'
+        },
         'sass-loader'
       ]
     }, {
